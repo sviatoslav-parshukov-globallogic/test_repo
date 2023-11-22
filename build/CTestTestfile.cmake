3e00a433d6f95164c46249866aac8d5c0cfd82d3
sviatoslav-parshukov-globallogic <147646978+sviatoslav-parshukov-globallogic@users.noreply.github.com>
2023-11-22 15:20:53 +0200

    Update main.yml

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user1/Documents/test_repo
-# Build directory: /home/user1/Documents/test_repo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
